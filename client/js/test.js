313125cdcb64c0a8fcddfdfbd342643c6c35dc37
João Sequeira <joaosequeira28@hotmail.com>
2022-05-02 17:06:46 +0100

    teste

--- client/js/test.js
+++ client/js/test.js
@@ -1,5 +1,6 @@
 var checkBox1 = document.getElementById("checkBox1");
 var formControl1 = document.getElementById("formControl1");
+var inputBox1 = document.getElementById("input");
 var flag = 0;
 
 
@@ -8,6 +9,7 @@ checkBox1.addEventListener("click", () => {
         checkBox1.classList.add("bg-yellow");
         checkBox1.classList.remove("bg-checkBox");
         formControl1.classList.add("text-white");
+        inputBox1.style.appearance = 
         flag++;
     } else {
         flag = 0;
