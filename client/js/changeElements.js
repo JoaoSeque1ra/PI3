6ce44608f556ef5e7bd9fe39df9976d0593a8ed6
João Sequeira <joaosequeira28@hotmail.com>
2022-05-03 12:39:07 +0100

    final do btn

--- client/js/changeElements.js
+++ client/js/changeElements.js
@@ -1,11 +1,9 @@
 var checkBox = document.getElementsByClassName("input-group");
 var formControl = document.getElementsByClassName("form-control");
 var inputBox = document.getElementsByClassName("changeInput");
-var flag = 0;
-var flag1 = 0;
-var flag2 = 0;
-var flag3 = 0;
-var flag4 = 0;
+var flag = 0, flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0;
+var getBody = document.getElementsByTagName("body");
+var bgColor = getBody[0].getAttribute("id"); 
 
 function cleanCheckBox() {
     for(let i = 0; i < inputBox.length; i++) {
@@ -17,50 +15,50 @@ cleanCheckBox();
 
 checkBox[0].addEventListener("click", () => {
     if(flag === 0) {
-        turnOn(0, "bg-yellow");
+        turnOn(0, bgColor);
         flag++;
     } else {
-        turnOff(0, "bg-yellow");
+        turnOff(0, bgColor);
         flag = 0;
     }
 });
 
 checkBox[1].addEventListener("click", () => {
     if(flag1 === 0) {
-        turnOn(1, "bg-yellow");
+        turnOn(1, bgColor);
         flag1++;
     } else {
-        turnOff(1, "bg-yellow");
+        turnOff(1, bgColor);
         flag1 = 0;
     }
 });
 
 checkBox[2].addEventListener("click", () => {
     if(flag2 === 0) {
-        turnOn(2, "bg-yellow");
+        turnOn(2, bgColor);
         flag2++;
     } else {
-        turnOff(2, "bg-yellow");
+        turnOff(2, bgColor);
         flag2 = 0;
     }
 });
 
 checkBox[3].addEventListener("click", () => {
     if(flag3 === 0) {
-        turnOn(3, "bg-yellow");
+        turnOn(3, bgColor);
         flag3++;
     } else {
-        turnOff(3, "bg-yellow");
+        turnOff(3, bgColor);
         flag3 = 0;
     }
 });
 
 checkBox[4].addEventListener("click", () => {
     if(flag4 === 0) {
-        turnOn(4, "bg-yellow");
+        turnOn(4, bgColor);
         flag4++;
     } else {
-        turnOff(4, "bg-yellow");
+        turnOff(4, bgColor);
         flag4 = 0;
     }
 });
@@ -74,7 +72,7 @@ function turnOn(index, bgColor) {
 
 function turnOff(index, bgColor) {
     checkBox[index].classList.add(bgColor);
-    checkBox[index].classList.remove("bg-yellow");
+    checkBox[index].classList.remove(bgColor);
     formControl[index].classList.remove("text-white");
     inputBox[index].checked = false;
 }
